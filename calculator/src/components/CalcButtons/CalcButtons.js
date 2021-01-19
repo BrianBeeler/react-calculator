01d4ae91fd45bd9826ec546360ae36b5d457ed44
BrianBeeler <root@Brians-MacBook-Pro.local>
2021-01-19 09:31:39 -0800

    Correct button labels.

--- calculator/src/components/CalcButtons/CalcButtons.js
+++ calculator/src/components/CalcButtons/CalcButtons.js
@@ -5,33 +5,33 @@ import styles from './CalcButtons.module.css';
 const CalcButtons = () => (
   <div className={styles.CalcButtons}>
     <div class="btn-group">
-      <button type="button" class="lg-button">AC</button>
-      <button type="button" class="lg-button">AC</button>
-      <button type="button" class="lg-button">AC</button>
-      <button type="button" class="y-button">AC</button>
+      <button type="button" class="lg-button">C</button>
+      <button type="button" class="lg-button">+/-</button>
+      <button type="button" class="lg-button">%</button>
+      <button type="button" class="y-button">/</button>
     </div>
     <div class="btn-group">
-      <button type="button" class="dg-button">AC</button>
-      <button type="button" class="dg-button">AC</button>
-      <button type="button" class="dg-button">AC</button>
-      <button type="button" class="y-button">AC</button>
+      <button type="button" class="dg-button">7</button>
+      <button type="button" class="dg-button">8</button>
+      <button type="button" class="dg-button">9</button>
+      <button type="button" class="y-button">X</button>
     </div>
     <div class="btn-group">
-      <button type="button" class="dg-button">AC</button>
-      <button type="button" class="dg-button">AC</button>
-      <button type="button" class="dg-button">AC</button>
-      <button type="button" class="y-button">AC</button>
+      <button type="button" class="dg-button">4</button>
+      <button type="button" class="dg-button">5</button>
+      <button type="button" class="dg-button">6</button>
+      <button type="button" class="y-button">-</button>
     </div>
     <div class="btn-group">
-      <button type="button" class="dg-button">AC</button>
-      <button type="button" class="dg-button">AC</button>
-      <button type="button" class="dg-button">AC</button>
-      <button type="button" class="y-button">AC</button>
+      <button type="button" class="dg-button">1</button>
+      <button type="button" class="dg-button">2</button>
+      <button type="button" class="dg-button">3</button>
+      <button type="button" class="y-button">+</button>
     </div>
     <div class="btn-group">
-      <button type="button" class="dg-button-long">AC</button>
-      <button type="button" class="dg-button">AC</button>
-      <button type="button" class="y-button">AC</button>
+      <button type="button" class="dg-button-long">0</button>
+      <button type="button" class="dg-button">.</button>
+      <button type="button" class="y-button">=</button>
     </div>
 
   </div>
