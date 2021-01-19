5a0f1246991e1abc4b6caddc04055f0df9363df3
BrianBeeler <root@Brians-MacBook-Pro.local>
2021-01-19 08:46:17 -0800

    Added calculator buttons.

--- calculator/src/components/CalcButtons/CalcButtons.js
+++ calculator/src/components/CalcButtons/CalcButtons.js
@@ -4,7 +4,36 @@ import styles from './CalcButtons.module.css';
 
 const CalcButtons = () => (
   <div className={styles.CalcButtons}>
-    CalcButtons Component
+    <div class="btn-group">
+      <button type="button" class="lg-button">AC</button>
+      <button type="button" class="lg-button">AC</button>
+      <button type="button" class="lg-button">AC</button>
+      <button type="button" class="y-button">AC</button>
+    </div>
+    <div class="btn-group">
+      <button type="button" class="dg-button">AC</button>
+      <button type="button" class="dg-button">AC</button>
+      <button type="button" class="dg-button">AC</button>
+      <button type="button" class="y-button">AC</button>
+    </div>
+    <div class="btn-group">
+      <button type="button" class="dg-button">AC</button>
+      <button type="button" class="dg-button">AC</button>
+      <button type="button" class="dg-button">AC</button>
+      <button type="button" class="y-button">AC</button>
+    </div>
+    <div class="btn-group">
+      <button type="button" class="dg-button">AC</button>
+      <button type="button" class="dg-button">AC</button>
+      <button type="button" class="dg-button">AC</button>
+      <button type="button" class="y-button">AC</button>
+    </div>
+    <div class="btn-group">
+      <button type="button" class="dg-button-long">AC</button>
+      <button type="button" class="dg-button">AC</button>
+      <button type="button" class="y-button">AC</button>
+    </div>
+
   </div>
 );
 
