6d3b5d7fe7460d494b0cbd0d25c5256cf43e0267
Brian Beeler <brianlbeeler@gmail.com>
2021-01-20 13:32:54 -0800

    Changed calculator component from functional to object-based component.

--- calculator/src/components/CalcButtons/CalcButtons.js
+++ calculator/src/components/CalcButtons/CalcButtons.js
@@ -1,49 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './CalcButtons.module.css';
 import { getElementError } from '@testing-library/react';
 
-const CalcButtons = () => (
-  <div className={ styles['parent-of-buttons'] } >
+class CalcButtons extends Component {
 
-    {/* Row 1 */}
-    <div className={ styles['btn-group'] }>
-      <button type="button" className={ styles['lg-button']} onClick={allClear}>AC</button>
-      <button type="button" className={ styles['lg-button']} onClick={reverseSign}>+/-</button>
-      <button type="button" className={ styles['lg-button']} onClick={applyModulus}>%</button>
-      <button type="button" className={ styles['y-button']} onClick={applyDevision}>÷</button>
-    </div>
-    
-    {/* Row 2 */}
-    <div className={ styles['btn-group'] }>
-      <button type="button" className={ styles['dg-button']} onClick={enterNumber} >7</button>
-      <button type="button" className={ styles['dg-button']} onClick={enterNumber } >8</button>
-      <button type="button" className={ styles['dg-button']} onClick={enterNumber } >9</button>
-      <button type="button" className={ styles['y-button']}  onClick={applyMultiplication}>X</button>
-    </div>
-    <div className={ styles['btn-group'] }>
-      <button type="button" className={ styles['dg-button'] }  onClick={enterNumber}>4</button>
-      <button type="button" className={ styles['dg-button'] } onClick={enterNumber}>5</button>
-      <button type="button" className={ styles['dg-button'] } onClick={enterNumber}>6</button>
-      <button type="button" className={ styles['y-button']  }  onClick={applySubtraction}>-</button>
-    </div>
+  render() {
+    return (
 
-    <div className={ styles['btn-group'] }>
-      <button type="button" className={ styles['dg-button']} onClick={enterNumber}>1</button>
-      <button type="button" className={ styles['dg-button']} onClick={enterNumber}>2</button>
-      <button type="button" className={ styles['dg-button']} onClick={enterNumber}>3</button>
-      <button type="button" className={ styles['y-button']} onClick={applyAddition}>+</button>
-    </div>
-    
-    <div className={ styles['btn-group'] }>
-      <button type="button" className={ styles['dg-button', 'double-long']} onClick={enterNumber}>0</button>
-      <button type="button" className={ styles['dg-button']} onClick={addDecimal}>.</button>
-      <button type="button" className={ styles['y-button']} onClick={applyEquals}>=</button>
-    </div>
+      <div className={ styles['parent-of-buttons'] } >
 
-  </div>  
+        {/* Row 1 */}
+        <div className={ styles['btn-group'] }>
+          <button type="button" className={ styles['lg-button']} onClick={allClear}>AC</button>
+          <button type="button" className={ styles['lg-button']} onClick={reverseSign}>+/-</button>
+          <button type="button" className={ styles['lg-button']} onClick={applyModulus}>%</button>
+          <button type="button" className={ styles['y-button']} onClick={applyDevision}>÷</button>
+        </div>
+        
+        {/* Row 2 */}
+        <div className={ styles['btn-group'] }>
+          <button type="button" className={ styles['dg-button']} onClick={enterNumber} >7</button>
+          <button type="button" className={ styles['dg-button']} onClick={enterNumber } >8</button>
+          <button type="button" className={ styles['dg-button']} onClick={enterNumber } >9</button>
+          <button type="button" className={ styles['y-button']}  onClick={applyMultiplication}>X</button>
+        </div>
+        <div className={ styles['btn-group'] }>
+          <button type="button" className={ styles['dg-button'] }  onClick={enterNumber}>4</button>
+          <button type="button" className={ styles['dg-button'] } onClick={enterNumber}>5</button>
+          <button type="button" className={ styles['dg-button'] } onClick={enterNumber}>6</button>
+          <button type="button" className={ styles['y-button']  }  onClick={applySubtraction}>-</button>
+        </div>
 
-);
+        <div className={ styles['btn-group'] }>
+          <button type="button" className={ styles['dg-button']} onClick={enterNumber}>1</button>
+          <button type="button" className={ styles['dg-button']} onClick={enterNumber}>2</button>
+          <button type="button" className={ styles['dg-button']} onClick={enterNumber}>3</button>
+          <button type="button" className={ styles['y-button']} onClick={applyAddition}>+</button>
+        </div>
+        
+        <div className={ styles['btn-group'] }>
+          <button type="button" className={ styles['dg-button', 'double-long']} onClick={enterNumber}>0</button>
+          <button type="button" className={ styles['dg-button']} onClick={addDecimal}>.</button>
+          <button type="button" className={ styles['y-button']} onClick={applyEquals}>=</button>
+        </div>
+
+      </div>  
+    )
+  }
+};
 
 // Click Handlers
 
