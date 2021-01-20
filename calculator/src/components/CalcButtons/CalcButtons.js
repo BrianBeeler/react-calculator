a23ac0ae0ddeda70d80f5f928fbb19e8ec1a2383
Brian Beeler <brianlbeeler@gmail.com>
2021-01-20 07:21:05 -0800

    Move updates to new directory.

--- calculator/src/components/CalcButtons/CalcButtons.js
+++ calculator/src/components/CalcButtons/CalcButtons.js
@@ -3,39 +3,42 @@ import PropTypes from 'prop-types';
 import styles from './CalcButtons.module.css';
 
 const CalcButtons = () => (
-  <div className={styles.CalcButtons}>
-    <div class="btn-group">
-      <button type="button" class="lg-button">C</button>
-      <button type="button" class="lg-button">+/-</button>
-      <button type="button" class="lg-button">%</button>
-      <button type="button" class="y-button">/</button>
+  <div className={ styles['parent-of-buttons'] } >
+    <div className={ styles['btn-group'] }>
+      <button type="button" className={ styles['lg-button'] }>C</button>
+      <button type="button" className={ styles['lg-button'] }>+/-</button>
+      <button type="button" className={ styles['lg-button'] }>%</button>
+      <button type="button" className={ styles['y-button']  }>/</button>
     </div>
-    <div class="btn-group">
-      <button type="button" class="dg-button">7</button>
-      <button type="button" class="dg-button">8</button>
-      <button type="button" class="dg-button">9</button>
-      <button type="button" class="y-button">X</button>
+    <div className={ styles['btn-group'] }>
+      <button type="button" className={ styles['dg-button'] }>7</button>
+      <button type="button" className={ styles['dg-button'] } >8</button>
+      <button type="button" className={ styles['dg-button'] }>9</button>
+      <button type="button" className={ styles['y-button'] }>X</button>
     </div>
-    <div class="btn-group">
-      <button type="button" class="dg-button">4</button>
-      <button type="button" class="dg-button">5</button>
-      <button type="button" class="dg-button">6</button>
-      <button type="button" class="y-button">-</button>
+    <div className={ styles['btn-group'] }>
+      <button type="button" className={ styles['dg-button'] }>4</button>
+      <button type="button" className={ styles['dg-button'] }>5</button>
+      <button type="button" className={ styles['dg-button'] }>6</button>
+      <button type="button" className={ styles['y-button'] }>-</button>
     </div>
-    <div class="btn-group">
-      <button type="button" class="dg-button">1</button>
-      <button type="button" class="dg-button">2</button>
-      <button type="button" class="dg-button">3</button>
-      <button type="button" class="y-button">+</button>
+    <div className={ styles['btn-group'] }>
+      <button type="button" className={ styles['dg-button']}>1</button>
+      <button type="button" className={ styles['dg-button']}>2</button>
+      <button type="button" className={ styles['dg-button']}>3</button>
+      <button type="button" className={ styles['y-button']}>+</button>
     </div>
-    <div class="btn-group">
-      <button type="button" class="dg-button-long">0</button>
-      <button type="button" class="dg-button">.</button>
-      <button type="button" class="y-button">=</button>
+    <div className={ styles['btn-group'] }>
+      <button type="button" className={ styles['dg-button']}>0</button>
+      <button type="button" className={ styles['dg-button']}>.</button>
+      <button type="button" className={ styles['y-button']}>=</button>
     </div>
 
   </div>
 );
+
+
+
 
 CalcButtons.propTypes = {};
 
