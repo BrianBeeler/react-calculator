153e57e1aacdc3a2fe3d0193401f85bf2be4f154
Brian Beeler <brianlbeeler@gmail.com>
2021-01-20 08:05:19 -0800

    Add button handlers.

--- calculator/src/components/CalcButtons/CalcButtons.js
+++ calculator/src/components/CalcButtons/CalcButtons.js
@@ -1,39 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './CalcButtons.module.css';
+import { getElementError } from '@testing-library/react';
 
 const CalcButtons = () => (
   <div className={ styles['parent-of-buttons'] } >
     <div className={ styles['btn-group'] }>
-      <button type="button" className={ styles['lg-button'] }>AC</button>
-      <button type="button" className={ styles['lg-button'] }>+/-</button>
-      <button type="button" className={ styles['lg-button'] }>%</button>
-      <button type="button" className={ styles['y-button']  }>÷</button>
+      <button type="button" className={ styles['lg-button']} onClick={allClear}>AC</button>
+      <button type="button" className={ styles['lg-button']} onClick={reverseSign}>+/-</button>
+      <button type="button" className={ styles['lg-button']} onClick={applyModulus}>%</button>
+      <button type="button" className={ styles['y-button']} onClick={applyAddition}>÷</button>
     </div>
     <div className={ styles['btn-group'] }>
-      <button type="button" className={ styles['dg-button'] }>7</button>
-      <button type="button" className={ styles['dg-button'] } >8</button>
-      <button type="button" className={ styles['dg-button'] }>9</button>
-      <button type="button" className={ styles['y-button'] }>X</button>
+      <button type="button" className={ styles['dg-button']} onClick={enterNumber} }>7</button>
+      <button type="button" className={ styles['dg-button']} onClick={enterNumber } } >8</button>
+      <button type="button" className={ styles['dg-button']} onClick={enterNumber } }>9</button>
+      <button type="button" className={ styles['y-button']}  onClick={applyMultiplication }>X</button>
     </div>
     <div className={ styles['btn-group'] }>
-      <button type="button" className={ styles['dg-button'] }>4</button>
-      <button type="button" className={ styles['dg-button'] }>5</button>
-      <button type="button" className={ styles['dg-button'] }>6</button>
-      <button type="button" className={ styles['y-button'] }>-</button>
+      <button type="button" className={ styles['dg-button'] }  onClick={enterNumber}>4</button>
+      <button type="button" className={ styles['dg-button'] } onClick={enterNumber}>5</button>
+      <button type="button" className={ styles['dg-button'] } onClick={enterNumber}>6</button>
+      <button type="button" className={ styles['y-button']  }  onClick={applySubtraction()}>-</button>
     </div>
     <div className={ styles['btn-group'] }>
-      <button type="button" className={ styles['dg-button']}>1</button>
-      <button type="button" className={ styles['dg-button']}>2</button>
-      <button type="button" className={ styles['dg-button']}>3</button>
-      <button type="button" className={ styles['y-button']}>+</button>
+      <button type="button" className={ styles['dg-button']} onClick={enterNumber}>1</button>
+      <button type="button" className={ styles['dg-button']} onClick={enterNumber}>2</button>
+      <button type="button" className={ styles['dg-button']} onClick={enterNumber}>3</button>
+      <button type="button" className={ styles['y-button']} onClick={applyAddition}>+</button>
     </div>
     <div className={ styles['btn-group'] }>
-      <button type="button" className={ styles['dg-button']}>0</button>
-      <button type="button" className={ styles['dg-button']}>.</button>
-      <button type="button" className={ styles['y-button']}>=</button>
+      <button type="button" className={ styles['dg-button']} onClick={enterNumber}>0</button>
+      <button type="button" className={ styles['dg-button']} onClick={addDecimal}>.</button>
+      <button type="button" className={ styles['y-button']} onClick={applyEquals}>=</button>
     </div>
-
   </div>
 );
 
